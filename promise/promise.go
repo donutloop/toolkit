bef93de1b80f032ff9f5acbcacbf1f7f0d62867e
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2017-09-23 11:54:49 +0200

    - Added: Promise implementation

---
+++ promise/promise.go
@@ -0,0 +1,29 @@
+package promise
+
+import (
+	"context"
+	"fmt"
+)
+
+//Do is a basic promise implementation: it wraps calls a function in a goroutine
+func Do(ctx context.Context, f func(ctx context.Context) error) (<-chan struct{}, chan error) {
+	done := make(chan struct{}, 1)
+	errc := make(chan error)
+	go func(done chan struct{}, error chan error, ctx context.Context) {
+		defer func() {
+			if v := recover(); v != nil {
+				errc <- fmt.Errorf("promise is panicked (%v)", v)
+			}
+		}()
+
+		if err := f(ctx); err != nil {
+			errc <- err
+			return
+		}
+
+		done <- struct{}{}
+	}(done, errc, ctx)
+
+
+	return done, errc
+}
