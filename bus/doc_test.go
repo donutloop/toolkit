2967877b155c7cc4cc9cc7e0cabd0696648bd272
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2017-09-30 11:03:01 +0200

    - Added: bus handler example test

--- bus/doc_test.go
+++ bus/doc_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Creates a bus and adds a listener to a message afterward it publishes a new message
-func ExampleBus() {
+func ExampleBusListener() {
 
 	type msg struct {
 		Id   int64
@@ -21,6 +21,28 @@ func ExampleBus() {
 	})
 
 	if err := b.Publish(new(msg)); err != nil {
+		fmt.Println(fmt.Sprintf("bus: %v", err))
+	}
+
+	// Output: db insert listener
+}
+
+// Creates a bus and adds a handler for a message afterward it dispatch a new message
+func ExampleBusHandler() {
+
+	type msg struct {
+		Id   int64
+		body string
+	}
+
+	b := bus.New()
+
+	b.AddHandler(func(m *msg) error {
+		fmt.Println("db insert listener")
+		return nil
+	})
+
+	if err := b.Dispatch(new(msg)); err != nil {
 		fmt.Println(fmt.Sprintf("bus: %v", err))
 	}
 
