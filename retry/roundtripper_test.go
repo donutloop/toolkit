5dda9d7e411e2539118908e117639f21cccaed99
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2019-10-27 18:18:00 +0100

    Add test case for roundtripper to response and request body content

--- retry/roundtripper_test.go
+++ retry/roundtripper_test.go
@@ -1,6 +1,8 @@
 package retry
 
 import (
+	"bytes"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"sync/atomic"
@@ -100,5 +102,71 @@ func TestRoundTripper_RoundTripStatusOk(t *testing.T) {
 
 	if counter != 1 {
 		t.Errorf("counter is bad, got=%v, want=%v", counter, 1)
+	}
+}
+
+
+func TestRoundTripper_RoundTripJsonStatusOk(t *testing.T) {
+
+	json := `{"hello":"world"}`
+
+	var counter int32
+	testserver := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		atomic.AddInt32(&counter, 1)
+		t.Log("hit endpoint")
+
+		b , err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		t.Log(string(b))
+
+		count := atomic.LoadInt32(&counter)
+		if count == 1 {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+
+		if string(b) != json {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(json))
+	}))
+
+	retryRoundTripper := NewRoundTripper(http.DefaultTransport, .50 , .15 , 3, nil, new(Exp))
+	httpClient := new(http.Client)
+	httpClient.Transport = retryRoundTripper
+
+	req, err := http.NewRequest(http.MethodGet, testserver.URL, bytes.NewBuffer([]byte(json)))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("response is bad, got=%v", resp.StatusCode)
+	}
+
+	if counter != 2 {
+		t.Errorf("counter is bad, got=%v, want=%v", counter, 1)
+	}
+
+	b , err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("response is bad, got=%v", err)
+	}
+
+	if string(b) != json {
+		t.Fatalf("response body is bad, got=%v", string(b))
 	}
 }
